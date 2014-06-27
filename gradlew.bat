4be884fccb817d543aaf68d996ee9f910945eda4
Eric Edens <eric.edens@gmail.com>
2014-06-27 14:43:09 -0700

    Add addl licenses

--- gradlew.bat
+++ gradlew.bat
@@ -1,7 +1,3 @@
-@rem This Source Code Form is subject to the terms of the Mozilla Public
-@rem License, v. 2.0. If a copy of the MPL was not distributed with this
-@rem file, You can obtain one at http://mozilla.org/MPL/2.0/.
-
 @if "%DEBUG%" == "" @echo off
 @rem ##########################################################################
 @rem
