e295d8a92a153d3672094550bad173d96da8b387
Eric Edens <eric.edens@gmail.com>
2014-07-02 14:28:45 -0700

    Fixes

--- app/src/main/java/org/mozilla/search/MainActivity.java
+++ app/src/main/java/org/mozilla/search/MainActivity.java
@@ -7,12 +7,9 @@ package org.mozilla.search;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.v4.app.FragmentActivity;
-import android.support.v4.app.FragmentManager;
-import android.support.v4.app.FragmentTransaction;
+import android.view.View;
 
 import org.mozilla.search.autocomplete.AcceptsSearchQuery;
-import org.mozilla.search.autocomplete.SearchFragment;
-
 
 /**
  * The main entrance for the Android search intent.
@@ -22,78 +19,52 @@ import org.mozilla.search.autocomplete.SearchFragment;
  * now, the only message passing occurs when a user wants to submit a search query. That
  * passes through the onSearch method here.
  */
-public class MainActivity extends FragmentActivity implements AcceptsSearchQuery,
-        FragmentManager.OnBackStackChangedListener {
+public class MainActivity extends FragmentActivity implements AcceptsSearchQuery {
 
-    private PostSearchFragment detailActivity;
+    enum State {
+        START,
+        PRESEARCH,
+        POSTSEARCH
+    }
+
+    private State state = State.START;
 
     @Override
     protected void onCreate(Bundle stateBundle) {
         super.onCreate(stateBundle);
-
-        // Sets the content view for the Activity
         setContentView(R.layout.search_activity_main);
-
-        // Gets an instance of the support library FragmentManager
-        FragmentManager localFragmentManager = getSupportFragmentManager();
-
-        // If the incoming state of the Activity is null, sets the initial view to be thumbnails
-        if (null == stateBundle) {
-
-            // Starts a Fragment transaction to track the stack
-            FragmentTransaction localFragmentTransaction = localFragmentManager.beginTransaction();
-
-            localFragmentTransaction.add(R.id.header_fragments, new SearchFragment(),
-                    Constants.SEARCH_FRAGMENT);
-
-            localFragmentTransaction.add(R.id.presearch_fragments, new PreSearchFragment(),
-                    Constants.PRESEARCH_FRAGMENT);
-
-            // Commits this transaction to display the Fragment
-            localFragmentTransaction.commit();
-        }
-    }
-
-    @Override
-    protected void onStart() {
-        super.onStart();
-
-        if (null == detailActivity) {
-            detailActivity = new PostSearchFragment();
-        }
-
-        if (null == getSupportFragmentManager().findFragmentByTag(Constants.POSTSEARCH_FRAGMENT)) {
-            FragmentTransaction txn = getSupportFragmentManager().beginTransaction();
-            txn.add(R.id.gecko_fragments, detailActivity, Constants.POSTSEARCH_FRAGMENT);
-            txn.hide(detailActivity);
-
-            txn.commit();
-        }
+        startPresearch();
     }
 
     @Override
     public void onSearch(String s) {
-        FragmentManager localFragmentManager = getSupportFragmentManager();
-        FragmentTransaction localFragmentTransaction = localFragmentManager.beginTransaction();
-
-        localFragmentTransaction
-                .hide(localFragmentManager.findFragmentByTag(Constants.POSTSEARCH_FRAGMENT))
-                .addToBackStack(null);
-
-        localFragmentTransaction
-                .show(localFragmentManager.findFragmentByTag(Constants.POSTSEARCH_FRAGMENT))
-                .addToBackStack(null);
-
-        localFragmentTransaction.commit();
-
-
-        ((PostSearchFragment) getSupportFragmentManager()
-                .findFragmentByTag(Constants.POSTSEARCH_FRAGMENT))
+        startPostsearch();
+        ((PostSearchFragment) getSupportFragmentManager().findFragmentById(R.id.gecko))
                 .setUrl("https://search.yahoo.com/search?p=" + Uri.encode(s));
     }
 
-    @Override
-    public void onBackStackChanged() {
+    private void startPresearch() {
+        if (state != State.PRESEARCH) {
+            state = State.PRESEARCH;
+            findViewById(R.id.gecko).setVisibility(View.INVISIBLE);
+            findViewById(R.id.presearch).setVisibility(View.VISIBLE);
+        }
+    }
 
+    private void startPostsearch() {
+        if (state != State.POSTSEARCH) {
+            state = State.POSTSEARCH;
+            findViewById(R.id.presearch).setVisibility(View.INVISIBLE);
+            findViewById(R.id.gecko).setVisibility(View.VISIBLE);
+        }
+    }
+
+    @Override
+    public void onBackPressed() {
+        if (state == State.POSTSEARCH) {
+            startPresearch();
+        } else {
+            super.onBackPressed();
+        }
     }
 }
