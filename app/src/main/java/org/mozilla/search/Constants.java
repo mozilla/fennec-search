fd342edcb0e59113fe010d81669e97eb54190d95
Eric Edens <eric.edens@gmail.com>
2014-07-02 16:23:03 -0700

    Update Constants.java

--- app/src/main/java/org/mozilla/search/Constants.java
+++ app/src/main/java/org/mozilla/search/Constants.java
@@ -15,9 +15,10 @@ package org.mozilla.search;
  * https://github.com/ericedens/FirefoxSearch/issues/3
  */
 public class Constants {
-    public static final String SEARCH_FRAGMENT = "org.mozilla.search.SEARCH_FRAGMENT";
+
     public static final String PRESEARCH_FRAGMENT = "org.mozilla.search.PRESEARCH_FRAGMENT";
     public static final String POSTSEARCH_FRAGMENT = "org.mozilla.search.POSTSEARCH_FRAGMENT";
+    public static final String SEARCH_FRAGMENT = "org.mozilla.search.SEARCH_FRAGMENT";
 
     public static final String AUTOCOMPLETE_ROW_LIMIT = "5";
 }
